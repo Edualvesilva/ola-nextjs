936163304aba048b16f33fb48868a1e7b09018c3
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2024-01-16 11:58:09 -0300

    Fazendo os links ficarem SPA

---
+++ src/pages/equipe.jsx
@@ -0,0 +1,20 @@
+import Menu from "@/components/Menu";
+
+export default function Equipe() {
+  return (
+    <>
+      <h1>Olá Next.js!</h1>;
+      <Menu />
+      <main>
+        <h2>Bem-vindo ao Next.js</h2>
+        <h3>Equipe</h3>
+        <p>Nossa equipe:</p>
+        <ol>
+          <li>Giuseppe</li>
+          <li>Lucas</li>
+          <li>Allan</li>
+        </ol>
+      </main>
+    </>
+  );
+}
---
+++ src/pages/servicos.jsx
@@ -0,0 +1,14 @@
+import Menu from "@/components/Menu";
+
+export default function Servicos() {
+  return (
+    <>
+      <h1>Olá Next.js!</h1>;
+      <Menu />
+      <main>
+        <h2>Bem-vindo ao Next.js</h2>
+        <h3>Serviços</h3>
+      </main>
+    </>
+  );
+}
